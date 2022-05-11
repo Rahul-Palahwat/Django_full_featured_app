0a46a43dfd5af477921bd601252345f6048c6d97
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-05-11 10:53:38 +0530

    Part-5 Database and migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+# for date time 
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+
+
+
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    # date_posted:models.DateTimeField(auto_now_add=True)
+    date_posted=models.DateTimeField(default=timezone.now)
+    # if the author is deleted from the database so to deal with the post of that user we use on_delete
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    # these are called magic methods or double undescore method 
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,31 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# to get the data from database 
+from .models import Post
+
 # Create your views here.
 
-posts=[ 
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post1',
-        'content':'First post content',
-        'date_posted':'August 27,2018'
-    },
-    {
-        'author':'Jane Doe',
-        'title':'Blog Post2',
-        'content':'Second post content',
-        'date_posted':'August 28,2018'
-    }
-]
+# posts=[ 
+#     {
+#         'author':'CoreyMS',
+#         'title':'Blog Post1',
+#         'content':'First post content',
+#         'date_posted':'August 27,2018'
+#     },
+#     {
+#         'author':'Jane Doe',
+#         'title':'Blog Post2',
+#         'content':'Second post content',
+#         'date_posted':'August 28,2018'
+#     }
+# ]
 
 
 def home(request):
     context={
-        'posts':posts
+        # 'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
