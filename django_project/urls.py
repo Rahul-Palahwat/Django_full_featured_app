752ed6b93516185200fcd7a0f4a56ef4de510de0
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-05-23 09:41:12 +0530

    ResetPassword then Hosting

--- django_project/urls.py
+++ django_project/urls.py
@@ -39,6 +39,15 @@ urlpatterns = [
     # now we will use auth views, as_view(parameter) this will tell where to look 
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
+
+    # for resetpassword 
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name="password_reset"),
+
+    # for mail perpose 
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name="password_reset_done"),
+
+
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name="password_reset_confirm"),
     
     path('profile/',user_views.profile,name="profile"),
 
